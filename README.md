035f45ed059a5d2ea4f4fd74bd61713b37f6b896
alanpilloud <alanp@rc2.ch>
2016-01-05 12:53:49 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# bwap-googlemaps
+Add a simple map with a unique marker
+
+## Usage
+After activation, use this shortcode :
+```
+[bwap-googlemaps key=YOURKEY lat=-25.363 lon=131.044 zoom=4 class=map]
+```
---
+++ bwap-googlemaps.php
@@ -0,0 +1,60 @@
+<?php
+/*
+Plugin Name: BWAP Googlemaps
+Plugin URI: http://alanpilloud.github.io
+Description: Add a simple map with a unique marker
+Version: 0.0.1
+Author: Bureau Web Alan Pilloud
+Author URI: http://alanpilloud.github.io
+*/
+
+defined( 'ABSPATH' ) or die;
+
+require 'plugin_update_check.php';
+$MyUpdateChecker = new PluginUpdateChecker_2_0 ('https://kernl.us/api/v1/updates/5680505a086e629607788b83/', __FILE__, 'bwap-googlemaps', 1);
+
+if (!class_exists('BwapGoogleMaps')) {
+
+    class BwapGoogleMaps
+    {
+        protected $tag = 'bwap-googlemaps';
+        protected $name = 'BWAP Googlemaps';
+        protected $version = '0.0.1';
+
+        public function __construct()
+        {
+            add_shortcode( $this->tag, array( &$this, 'shortcode' ) );
+        }
+
+        public function shortcode( $atts, $content = null )
+        {
+            $mapid = $this->tag;
+
+            $atts = shortcode_atts( array(
+                'key' => '',
+                'title' => '',
+                'lat' => '-25.363',
+                'lon' => '131.044',
+                'zoom' => 4,
+                'class' => 'map'
+            ), $atts );
+
+            add_action('wp_footer', function() use ($atts, $mapid) {
+                ?>
+                <script async defer src="https://maps.googleapis.com/maps/api/js?key=<?= $atts['key'] ?>&amp;callback=initMap"></script>
+                <script>
+                function initMap() {
+                  var myLatLng = {lat: <?= $atts['lat'] ?>, lng: <?= $atts['lon'] ?>};
+
+                  var map = new google.maps.Map(document.getElementById("<?= $mapid ?>"), {center: myLatLng,scrollwheel: false, zoom: <?= $atts['zoom'] ?>});
+
+                  var marker = new google.maps.Marker({map: map, position: myLatLng, title: "<?= $atts['title'] ?>" });
+              }</script>
+              <?php
+            });
+
+            echo '<div id="'.$mapid.'" class="'.$atts['class'].'"></div>';
+        }
+    }
+    new BwapGoogleMaps;
+}
